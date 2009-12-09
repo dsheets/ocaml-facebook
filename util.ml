f933817ea1d38758d5a052d84587a03433827713
David Sheets <sheets@sheets-ubuntu.(none)>
2009-12-08 18:41:16 -0800

    LGPL licensing

--- util.ml
+++ util.ml
@@ -1,3 +1,23 @@
+(*
+  ocaml-facebook - Facebook Platform client API in OCaml
+
+  Copyright (C) <2009> David Sheets <sheets@alum.mit.edu>
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU Library General Public License as
+  published by the Free Software Foundation, version 2.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU Library General Public License for more details.
+
+  You should have received a copy of the GNU Library General Public
+  License along with this program; if not, write to the Free Software
+  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307
+  USA
+*)
+
 open Lwt
 
 module Assoc(M : Map.S) =
